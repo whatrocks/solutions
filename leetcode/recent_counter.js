f3ff0ae460a50f96dfa06c6598db00b2fb6f590a
Charlie Harrington <charles.harrington@gmail.com>
2019-02-15 14:24:51 -0800

    add simplified RecentCounter

--- leetcode/recent_counter.js
+++ leetcode/recent_counter.js
@@ -1,35 +1,49 @@
+// OVERLY COMPLICATED SOLUTION THAT WORKS
+// var RecentCounter = function() {
+//     this.pings_queue = []
+//     this.recent_count = 0
+// }
+
+// RecentCounter.prototype.ping = function(t) {
+//     if (!t) {
+//         return
+//     }
+//     if (!this.pings_queue.length) {
+//         this.pings_queue.push(t)
+//         this.recent_count = 1
+//     }
+//     else if (t - this.pings_queue[0] <= 3000) {
+//         this.recent_count++
+//         this.pings_queue.push(t)
+//     } else {
+//         // remove the first item from the lsit
+//         this.pings_queue.shift()
+
+//         while (this.pings_queue.length) {
+//             if (t - this.pings_queue[0] <= 3000) {
+//                 break;
+//             } else {
+//                 this.pings_queue.shift()
+//                 this.recent_count--
+//             }
+//         }
+//         this.pings_queue.push(t)
+
+//     }
+//     return this.recent_count    
+// }
+ 
+// Time O(queryes)
 var RecentCounter = function() {
-    this.pings_queue = []
-    this.recent_count = 0
+    this.q = []
 }
 
 RecentCounter.prototype.ping = function(t) {
-    if (!t) {
-        return
+    this.q.push(t)
+    while (this.q[0] < t - 3000) {
+        this.q.shift()
     }
-    if (!this.pings_queue.length) {
-        this.pings_queue.push(t)
-        this.recent_count = 1
-    }
-    else if (t - this.pings_queue[0] <= 3000) {
-        this.recent_count++
-        this.pings_queue.push(t)
-    } else {
-        // remove the first item from the lsit
-        this.pings_queue.shift()
-
-        while (this.pings_queue.length) {
-            if (t - this.pings_queue[0] <= 3000) {
-                break;
-            } else {
-                this.pings_queue.shift()
-                this.recent_count--
-            }
-        }
-        this.pings_queue.push(t)
-
-    }
-    return this.recent_count    
+    return this.q.length
 }
 
 var rc = new RecentCounter()
