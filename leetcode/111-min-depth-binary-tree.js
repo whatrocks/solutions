2e7781c33cc0bbd605ed0827312f6cec25a9576e
Charlie Harrington <charles.harrington@gmail.com>
2019-02-19 19:11:02 -0800

    adjust

--- leetcode/111-min-depth-binary-tree.js
+++ leetcode/111-min-depth-binary-tree.js
@@ -10,31 +10,54 @@
  * @return {number}
  */
 var minDepth = function(root) {
+    // More explicit BFS approach
     if (!root) {
         return 0;
     }
-    if (!root.left && !root.right) {
-        return 1;
-    }
-    let min = 1;
-    let queue = [[root, 1]];
-
-    while (queue.length) {
-        let current = queue.shift();
-        let node = current[0];
-        let depth = current[1]
-        if (!node.left && !node.right) {
-            min = depth
-            break;
+    let depth = 1;
+    let level = [root];
+    while (True) {
+        let next_level = []
+        for (node of level) {
+            if (!node.left && !node.right) {
+                return depth
+            }
+            if (node.left) {
+                next_level.push(node.left)
+            }
+            if (node.right) {
+                next_level.push(node.right)
+            }
         }
-        if (node.left) {
-            queue.push([node.left, depth + 1])
-        }
-        if (node.right) {
-            queue.push([node.right, depth + 1])
-        }
+        level = next_level
+        depth += 1
     }
 
-    return min
+    // Original working answer
+    //     if (!root) {
+//         return 0;
+//     }
+//     if (!root.left && !root.right) {
+//         return 1;
+//     }
+//     let min = 1;
+//     let queue = [[root, 1]];
 
+//     while (queue.length) {
+//         let current = queue.shift();
+//         let node = current[0];
+//         let depth = current[1]
+//         if (!node.left && !node.right) {
+//             min = depth
+//             break;
+//         }
+//         if (node.left) {
+//             queue.push([node.left, depth + 1])
+//         }
+//         if (node.right) {
+//             queue.push([node.right, depth + 1])
+//         }
+//     }
+
+//     return min
 };
