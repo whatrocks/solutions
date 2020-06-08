027b7fbf17d1b1642f4aac004d8a2438c9e7f1be
Charlie Harrington <charles.harrington@gmail.com>
2020-06-07 18:03:35 -0700

    space age

--- exercism/go/space-age/space_age.go
+++ exercism/go/space-age/space_age.go
@@ -1,3 +1,20 @@
 package space
 
 type Planet string
+
+var (
+  planetMap = map[Planet]float64{
+    "Mercury": 0.2408467,
+    "Venus": 0.61519726,
+    "Earth": 1.0,
+    "Mars":  1.8808158,
+    "Jupiter": 11.862615,
+    "Saturn": 29.447498,
+    "Uranus": 84.016846,
+    "Neptune": 164.79132,
+  }
+)
+
+func Age(seconds float64, planet Planet) (planetYears float64) {
+  return (seconds / 31557600) / planetMap[planet]
+}
