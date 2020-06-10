1b8691664c7c276748769ace3ddce759e6ab1d72
Charlie Harrington <charles.harrington@gmail.com>
2020-06-10 07:41:52 -0700

    overwrite name

--- exercism/go/two-fer/two_fer.go
+++ exercism/go/two-fer/two_fer.go
@@ -5,7 +5,7 @@ import "fmt"
 // ShareWith takes a name and returns a messages
 func ShareWith(name string) string {
   if name == "" {
-    return ShareWith("you")
+    name = "you"
   }
   return fmt.Sprintf("One for %s, one for me.", name)
 }
