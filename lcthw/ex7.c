34d79dce3c677064f5fa9c4635d8a4702238ff77
Charlie Harrington <charles.harrington@gmail.com>
2020-08-12 14:51:45 -0700

    working on ex7

---
+++ lcthw/ex7.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+  int distance = 100;
+  float power = 2.345f;
+  double super_power = 56789.4532;
+  char initial = 'A';
+  char first_name[] = "Zed";
+  char last_name[] = "Shaw";
+
+  printf("You are %d miles away.\n", distance);
+  printf("You have %f levels of power.\n", power);
+
+
+}
