9cbc59a467ae0cc6a67954209ed5bfd3b9c1e72b
Charlie Harrington <charles.harrington@gmail.com>
2020-08-25 21:59:03 -0700

    ex12

--- lcthw/ex12.c
+++ lcthw/ex12.c
@@ -14,8 +14,16 @@ int main(int argc, char *argv[] ) {
   printf("The size of an int: %ld\n", sizeof(int));
   printf("The size of areas (int[]): %ld\n", sizeof(areas));
   printf("The number of ints in areas: %ld\n", sizeof(areas) / sizeof(int));
-
   printf("The first area is %d, the second area is %d.\n", areas[0], areas[1]);
+
+  printf("The size of a char: %ld\n", sizeof(char));
+  printf("The size of name (char[]): %ld\n", sizeof(name));
+  printf("The number of chars: %ld\n", sizeof(name) / sizeof(char));
+
+  printf("The size of full name (char[]): %ld\n", sizeof(full_name));
+  printf("The number of chars: %ld\n", sizeof(full_name) / sizeof(char));
+
+  printf("Name=\"%s\" and full_name=\"%s\"\n", name, full_name);
   
   return 0;
 }
