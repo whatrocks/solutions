676e4468b5cb02827400c2797170c2bd53261001
Charlie Harrington <charles.harrington@gmail.com>
2020-08-26 21:04:08 -0700

    ex13

--- lcthw/ex13.c
+++ lcthw/ex13.c
@@ -8,6 +8,18 @@ int main(int argc, char *argv[])
     printf("arg %d: %s\n", i, argv[i]);
   }
 
+  // let's make our own array of strings
+  char *states[] = {
+    "California", "Oregon",
+    "Washington", "Texas"
+  };
+
+  int num_states = 4;
+
+  for (i = 0; i < num_states; i++) {
+    printf("state %d: %s\n", i, states[i]);
+  }
+
   return 0;
 }
 
