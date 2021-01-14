c338ef572855b8114bde9c6354435e9362f6c62e
Charlie Harrington <charles.harrington@gmail.com>
2021-01-14 12:54:04 -0800

    add beginning of ex19

---
+++ lcthw/dbg.h
@@ -0,0 +1,14 @@
+#ifndef __dbg_h__
+#define __dbg_h__
+
+#include <stdio.h>
+#include <errno.h>
+#include <string.h>
+
+#ifdef NDEBUG
+#define debug(M, ...)
+#else
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
+#endif
+
+#endif
---
+++ lcthw/ex19.c
@@ -0,0 +1,8 @@
+#include "dbg.h"
+#include <stdio.h>
+
+int main(int argc, char* argv[]) {
+  printf("I love you.\n");
+  debug("what the heck do you mean?");
+  return 0;
+}
