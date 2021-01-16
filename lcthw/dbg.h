e7955eaa75ad71794df974b75aaeb6b079634b45
Charlie Harrington <charles.harrington@gmail.com>
2021-01-15 16:58:52 -0800

    fix dbg

--- lcthw/dbg.h
+++ lcthw/dbg.h
@@ -11,4 +11,9 @@
 #define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n", __FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
+#define clean_errno() (errno == 0 ? "None": strerror(errno))
+
+#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
+
+
 #endif
--- lcthw/ex19.c
+++ lcthw/ex19.c
@@ -1,8 +1,21 @@
 #include "dbg.h"
+#include <stdlib.h>
 #include <stdio.h>
 
+void test_debug() {
+  // notice that you don't need the \n
+  debug("I have Brown Hair.");
+  // passing arguments like printf
+  debug("I am %d years old.\n", 34);
+}
+
+void test_log_error() {
+  log_err("I believe everything is broken.");
+  log_err("There are %d problems in %s", 0, "space");
+}
+
 int main(int argc, char* argv[]) {
-  printf("I love you.\n");
-  debug("what the heck do you mean?");
+  test_debug();
+  test_log_error();
   return 0;
 }
