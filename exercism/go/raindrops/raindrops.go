314cb6d29f00e8af7622349f792e9038cf26d640
Charlie Harrington <charles.harrington@gmail.com>
2021-04-10 17:51:33 -0700

    gofmt raindrops

--- exercism/go/raindrops/raindrops.go
+++ exercism/go/raindrops/raindrops.go
@@ -10,13 +10,13 @@ const (
 
 func Convert(num int) string {
 	result := ""
-	if num % 3 == 0 {
+	if num%3 == 0 {
 		result += pling
 	}
-	if num % 5 == 0 {
+	if num%5 == 0 {
 		result += plang
 	}
-	if num % 7 == 0 {
+	if num%7 == 0 {
 		result += plong
 	}
 	if len(result) == 0 {
