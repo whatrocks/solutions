a2c17306abe749e2a66e9b4ae2c44660b319930e
Charlie Harrington <charles.harrington@gmail.com>
2021-04-13 19:51:58 -0700

    update leap

--- exercism/go/leap/leap.go
+++ exercism/go/leap/leap.go
@@ -1,18 +1,11 @@
-// This is a "stub" file.  It's a little start on your solution.
-// It's not a complete solution though; you have to write some code.
-
-// Package leap should have a package comment that summarizes what it's about.
-// https://golang.org/doc/effective_go.html#commentary
+// Package leaps determines if a year is a leap year.
 package leap
 
 // IsLeapYear determines if a year is a leap year.
 func IsLeapYear(year int) bool {
 	isLeap := false
-	if year % 4 == 0 {
+	if year%4 == 0 && !(year%100 == 0 && year%400 != 0) {
 		isLeap = true
-		if year % 100 == 0 && year % 400 != 0 {
-			isLeap = false
-		}
 	}
 	return isLeap
 }
