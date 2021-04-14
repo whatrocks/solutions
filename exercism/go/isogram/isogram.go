42529c33f362932a68b59b665705f4d92fdee250
Charlie Harrington <charles.harrington@gmail.com>
2021-04-14 07:10:50 -0700

    update isogram

--- exercism/go/isogram/isogram.go
+++ exercism/go/isogram/isogram.go
@@ -8,18 +8,16 @@ import "unicode"
 
 // IsIsogram determines if a word is an isogram.
 func IsIsogram(word string) bool {
-	if word == "" {
-		return true
-	}
 	cache := map[rune]bool{}
 	for _, char := range word {
 		if char == ' ' || char == '-' {
 			continue
 		}
-		if _, ok := cache[unicode.ToLower(char)]; ok {
+		letter := unicode.ToLower(char)
+		if _, ok := cache[letter]; ok {
 			return false
 		}
-		cache[unicode.ToLower(char)] = true
+		cache[letter] = true
 	}
 	return true
 }
