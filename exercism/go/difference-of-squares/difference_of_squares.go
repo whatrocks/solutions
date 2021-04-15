e7d32da152acbdd8bab26d17a781a4b751aa30b1
Charlie Harrington <charles.harrington@gmail.com>
2021-04-14 20:11:13 -0700

    update diff of squares

--- exercism/go/difference-of-squares/difference_of_squares.go
+++ exercism/go/difference-of-squares/difference_of_squares.go
@@ -4,24 +4,16 @@
 package diffsquares
 
 // SquareOfSum calculates the square of the sum of numbers.
-func SquareOfSum(number int) int {
-	sum := 0
-	for i := 1; i <= number; i++ {
-		sum += i
-	}
-	return sum * sum
+func SquareOfSum(n int) int {
+	return (n * (n + 1) / 2) * (n * (n + 1) / 2)
 }
 
 // SumOfSquares calculates the sum of the squares of numbers.
-func SumOfSquares(number int) int {
-	sum := 0
-	for i := 1; i <= number; i++ {
-		sum += i * i
-	}
-	return sum
+func SumOfSquares(n int) int {
+	return (n * (n + 1) * (2*n + 1)) / 6
 }
 
 // Difference calculates the difference of squares.
-func Difference(number int) int {
-	return SquareOfSum(number) - SumOfSquares(number)
+func Difference(n int) int {
+	return SquareOfSum(n) - SumOfSquares(n)
 }
