402a00ab6b3afa117015f4bd1774ef38a586e1a0
Charlie Harrington <charles.harrington@gmail.com>
2021-04-20 19:55:49 -0700

    update hamming in c

--- exercism/c/hamming/src/hamming.c
+++ exercism/c/hamming/src/hamming.c
@@ -3,11 +3,11 @@
 #include "hamming.h"
 
 int compute(const char *lhs, const char *rhs) {
-  int distance = 0;
   if (strlen(lhs) != strlen(rhs)) {
     return -1;
   }
-  for (int i = 0; lhs[i] != '\0'; i++) {
+  int distance = 0;
+  for (size_t i = 0; lhs[i] != '\0'; i++) {
     if (lhs[i] != rhs[i]) {
       distance++;
     }
