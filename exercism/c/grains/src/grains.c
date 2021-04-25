0baca8e0663ce7616a30bd30e6d12c0fdfdec219
Charlie Harrington <charles.harrington@gmail.com>
2021-04-25 13:25:03 -0700

    update grains

--- exercism/c/grains/src/grains.c
+++ exercism/c/grains/src/grains.c
@@ -4,10 +4,10 @@ uint64_t square(uint8_t index) {
   if (index < 1 || index > 64) {
     return 0;
   }
-  return 1L << (index - 1);
+  return 1ULL << (index - 1);
 }
 
 uint64_t total(void) {
-  return 0 - 1;
+  return -1;
 }
 
