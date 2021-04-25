aca39176adf7e0d8c22bda25cbe0ceb491429cfe
Charlie Harrington <charles.harrington@gmail.com>
2021-04-25 13:16:10 -0700

    improve total

--- exercism/c/grains/src/grains.c
+++ exercism/c/grains/src/grains.c
@@ -1,5 +1,4 @@
 #include "grains.h"
-#include <stdio.h>
 
 uint64_t square(uint8_t index) {
   if (index < 1 || index > 64) {
@@ -9,9 +8,6 @@ uint64_t square(uint8_t index) {
 }
 
 uint64_t total(void) {
-  uint64_t sum = 0;
-  for (int i = 1; i <= 64; i++) {
-    sum += square(i);
-  }
-  return sum;
+  return 0 - 1;
 }
+
