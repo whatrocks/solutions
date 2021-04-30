9dc1de6fd5f4d2d773fe6ec8a507cce6732b5b64
Charlie Harrington <charles.harrington@gmail.com>
2021-04-29 22:26:28 -0700

    square root in c

---
+++ exercism/c/square-root/src/square_root.c
@@ -0,0 +1,8 @@
+#include <math.h>
+#include <stdint.h>
+
+#include "square_root.h"
+
+uint16_t square_root(uint16_t number) {
+  return sqrt(number);
+}
---
+++ exercism/c/square-root/src/square_root.h
@@ -0,0 +1,8 @@
+#include <stdint.h>
+
+#ifndef SQUARE_ROOT_H
+#define SQUARE_ROOT_H
+
+uint16_t square_root(uint16_t number);
+
+#endif
