67337d39c04f6b8211e63ef28681d508320c1773
Charlie Harrington <charles.harrington@gmail.com>
2021-05-13 07:47:35 -0700

    update darts

--- exercism/c/darts/src/darts.c
+++ exercism/c/darts/src/darts.c
@@ -3,18 +3,15 @@
 
 #include "darts.h"
 
-bool is_inside_circle(coordinate_t pos, int radius) {
-  return sqrt(pow(pos[0], 2) + pow(pos[1], 2)) <= radius;
-}
-
 uint8_t score(coordinate_t pos) {
-  if (is_inside_circle(pos, 1)) {
+  float distance = sqrt(pow(pos[0], 2) + pow(pos[1], 2));
+  if (distance <= 1) {
     return 10; 
   }
-  if (is_inside_circle(pos, 5)) {
+  if (distance <= 5) {
     return 5;
   }
-  if (is_inside_circle(pos, 10)) {
+  if (distance <= 10) {
     return 1;
   }
   return 0;
