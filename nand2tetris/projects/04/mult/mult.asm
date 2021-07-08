bc00263fcef0b187fc4c4865d90a8f26d9a12d03
Charlie Harrington <charles.harrington@gmail.com>
2021-07-08 15:49:44 -0400

    Multiply assembly complete

--- nand2tetris/projects/04/mult/mult.asm
+++ nand2tetris/projects/04/mult/mult.asm
@@ -6,4 +6,33 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+// Usage: put a value>0 in RAM[0] and RAM[1]
+
+    // n = R1
+    @R1
+    D = M
+    @n
+    M = D
+    // initialize R2 to 0
+    @R2
+    M = 0
+(LOOP)
+    // if (n == 0) goto END
+    @n
+    D = M
+    @END
+    D;JEQ
+    // R2 = R2 + R0
+    @R0
+    D = M
+    @R2
+    M = M + D
+    // n--
+    @n
+    M = M - 1
+    // goto LOOP
+    @LOOP
+    0;JMP
+(END)
+    @END
+    0;JMP
