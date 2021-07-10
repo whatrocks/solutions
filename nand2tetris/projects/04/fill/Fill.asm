59bb9fd09b9f18a92f5c7405cfc25a73e20ae8d7
Charlie Harrington <charles.harrington@gmail.com>
2021-07-10 09:37:36 -0400

    Fill assembly script complete

--- nand2tetris/projects/04/fill/Fill.asm
+++ nand2tetris/projects/04/fill/Fill.asm
@@ -11,4 +11,52 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+@i
+M = 0
+(WHITE)
+    @KBD
+    D = M
+    @BLACK
+    D; JGT
+    // draw white
+    @SCREEN
+    D = A
+    @i
+    A = D + M
+    M = 0
+    // if i is JEQ, goto WHITE
+    @i
+    D = M
+    @WHITE
+    D; JEQ
+    // else decrement
+    @i
+    M = M - 1
+    // repeat loop
+    @WHITE
+    0; JMP
+(BLACK)
+    // check keyboard is pressed, otherwise goto WHITE
+    @KBD
+    D = M
+    @WHITE
+    D; JEQ
+    // check to make sure we haven't gone past screen memory
+    @i
+    D = M
+    @8191
+    D = A - D
+    @BLACK
+    D; JLT
+    // draw next word black
+    @SCREEN
+    D = A
+    @i
+    A = D + M
+    M = -1
+    // increment our counter
+    @i
+    M = M + 1
+    // repeat loop
+    @BLACK
+    0; JMP
