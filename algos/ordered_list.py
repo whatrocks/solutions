bdefc554c30eede9ed95dd600300444f49d34f51
Charlie Harrington <charles.harrington@gmail.com>
2021-11-08 14:26:09 -0500

    add lists

---
+++ algos/ordered_list.py
@@ -0,0 +1,30 @@
+from list import Node, UnorderedList
+
+class OrderedList(UnorderedList):
+    
+    def search(self, item):
+        current = self.head
+
+        while current is not None:
+            if current.value == item:
+                return True
+            if current.value > item:
+                return False
+            current = current.next
+
+        return False
+
+    def add(self, item):
+        current = self.head
+        previous = None
+
+        while current is not None:
+            if current.value > item:
+                break
+            previous, current = current, current.next
+
+            temp = Node(item)
+            if previous is None:
+                temp.next, self.head = self.head, temp
+            else:
+                temp.next, previous.next = current, temp
