6f7fb90ad33e560cc4637417c7520abad41dfc13
Charlie Harrington <charles.harrington@gmail.com>
2021-11-08 12:34:10 -0500

    hot potato

---
+++ algos/potato.py
@@ -0,0 +1,15 @@
+from collections import deque
+
+def hot_potato(names, num):
+    queue = deque()
+    for name in names:
+        queue.appendleft(name)
+    while len(queue) > 1:
+        for _ in range(num):
+            queue.appendleft(queue.pop())
+        queue.pop()
+    return queue.pop()
+
+
+print(hot_potato(('Bill', 'David', 'Susan', 'Jane', 'Kent', 'Brad'), 9))
+
