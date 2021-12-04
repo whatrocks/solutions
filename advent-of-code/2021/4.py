a7832c2549c66e0c692aebfa2c6a07aa3bdd5bdd
Charlie Harrington <charles.harrington@gmail.com>
2021-12-04 06:50:54 -0800

    day 4 part 2

--- advent-of-code/2021/4.py
+++ advent-of-code/2021/4.py
@@ -22,45 +22,98 @@ def mark_board(guess, board):
             if col == guess:
                 board[i][j] = None
 
-with open('input4.txt') as f:
-    lines = [x.rstrip() for x in f.readlines()]
+def part_one():
+    with open('input4.txt') as f:
+        lines = [x.rstrip() for x in f.readlines()]
 
-    guesses = []
-    boards = []
+        guesses = []
+        boards = []
 
-    current_board = []
-    for index, line in enumerate(lines):
-        if index == 0:
-            nums = line.split(',')
-            guesses = [int(x) for x in nums]
-        elif index == 1:
-            continue
-        elif line == '':
-            boards.append(current_board)
-            current_board = []
-        else:
-            split = line.split(' ')
-            cleaned = []
-            for n in split:
-                if len(n):
-                    cleaned.append(int(n))
-            current_board.append(cleaned)
-    boards.append(current_board)
-    
-    
-    found_winner = False
-    for guess in guesses:
-        if found_winner:
-            break
-        for board in boards:
-            mark_board(guess, board)
-            is_winner_horiz = check_horiz_win(board)
-            vert_transposed = [[board[col][row] for col in range(len(board))] for row in range(len(board[0]))]
-            is_winner_vert = check_horiz_win(vert_transposed)
-            if is_winner_horiz:
-                score = score_calc(guess, board)
-                print("score is: ", score)
-                found_winner = True
-                break
-            
+        current_board = []
+        for index, line in enumerate(lines):
+            if index == 0:
+                nums = line.split(',')
+                guesses = [int(x) for x in nums]
+            elif index == 1:
+                continue
+            elif line == '':
+                boards.append(current_board)
+                current_board = []
+            else:
+                split = line.split(' ')
+                cleaned = []
+                for n in split:
+                    if len(n):
+                        cleaned.append(int(n))
+                current_board.append(cleaned)
+        boards.append(current_board)
         
+        found_winner = False
+        score = 0
+        for guess in guesses:
+            if found_winner:
+                break
+            for board in boards:
+                mark_board(guess, board)
+                is_winner_horiz = check_horiz_win(board)
+                vert_transposed = [[board[col][row] for col in range(len(board))] for row in range(len(board[0]))]
+                is_winner_vert = check_horiz_win(vert_transposed)
+                if is_winner_horiz or is_winner_vert:
+                    score = score_calc(guess, board)
+                    print("score is: ", score)
+                    found_winner = True
+                    break
+        return score
+
+   
+print("PART ONE ANSWER: ", part_one())
+
+def part_two():
+    score = 0
+    with open('input4.txt') as f:
+        lines = [x.rstrip() for x in f.readlines()]
+
+        guesses = []
+        boards = []
+
+        current_board = []
+        for index, line in enumerate(lines):
+            if index == 0:
+                nums = line.split(',')
+                guesses = [int(x) for x in nums]
+            elif index == 1:
+                continue
+            elif line == '':
+                boards.append(current_board)
+                current_board = []
+            else:
+                split = line.split(' ')
+                cleaned = []
+                for n in split:
+                    if len(n):
+                        cleaned.append(int(n))
+                current_board.append(cleaned)
+        boards.append(current_board)
+
+
+        wins = [None for board in boards]
+        last_winner_idx = 0
+        for guess in guesses:
+            for idx, board in enumerate(boards):
+                mark_board(guess, board)
+                is_winner_horiz = check_horiz_win(board)
+                vert_transposed = [[board[col][row] for col in range(len(board))] for row in range(len(board[0]))]
+                is_winner_vert = check_horiz_win(vert_transposed)
+                if is_winner_horiz or is_winner_vert:
+                    score = score_calc(guess, board)
+                    print("score:" ,score)
+                    if wins[idx] == None:
+                        wins[idx] = score
+                        last_winner_idx = idx 
+    print(wins)                   
+    score = wins[last_winner_idx]
+    return score
+
+print("PART TWO ANSWER: ", part_two())
+
+
