eff2a5e666c24e8636a8eb142a5b2a134162d093
Charlie Harrington <charles.harrington@gmail.com>
2021-12-27 09:51:00 -0500

    more failure on day 14 part 2

--- advent-of-code/2021/14.py
+++ advent-of-code/2021/14.py
@@ -13,11 +13,15 @@ with open("input14.txt") as f:
     print(template)
     print(rules)
 
-    for step in range(40):
+    """
+    # works for part one
+    for step in range(6):
+        print(step, template, len(template))
+        length = len(template)
         next = ''
-        for i, _ in enumerate(template):
+        for i in range(length):
             first = template[i]
-            if i + 1 > len(template) - 1:
+            if i + 1 > length - 1:
                 next += first
             else:
                 second = template[i+1]
@@ -32,8 +36,50 @@ with open("input14.txt") as f:
             counts[c] += 1
         else:
             counts[c] = 1
+    """
+
+    import collections
+    # part two
+    counts = {}
+    q = collections.deque()
+
+    for i, c in enumerate(template):
+        if c in counts:
+            counts[c] += 1
+        else:
+            counts[c] = 1
+        if i == len(template) - 1:
+            break
+        first_letter = template[i]
+        second_letter = template[i + 1]
+        combined = first_letter + second_letter
+        q.appendleft(combined)
+
+    # print(q)
+
+    for step in range(40):
+        print(step)
+        next_queue = collections.deque()
+        while len(q):
+            current = q.pop()
+            next = rules[current]
+            if next in counts:
+                counts[next] += 1
+            else:
+                counts[next] = 1
+            next_queue.appendleft(current[0] + next)
+            next_queue.appendleft(next + current[1])
+        # print("next: ", next_queue)
+        q = next_queue
+
+    # l = 20
+    # add = l - 1
+    # for _ in range(40):
+    #     l  += add
+    #     add *= 2
+    # print("L : ", l)
     
-    print(counts)
+    # print(counts)
     sorted_counts = sorted(counts.values())
     print(sorted_counts[len(sorted_counts) - 1] - sorted_counts[0])
 
