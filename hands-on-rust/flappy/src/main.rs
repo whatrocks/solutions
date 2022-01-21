a7ad21e1a9b28fd8f6c19602d8af7a25b9ab9b7e
Charlie Harrington <charles.harrington@gmail.com>
2022-01-21 06:41:50 -0800

    starting flappy bird clone

---
+++ hands-on-rust/flappy/src/main.rs
@@ -0,0 +1,14 @@
+use bracket_lib::prelude::*;
+
+struct State {}
+
+impl GameState for State {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, Bracket Terminal!");
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
