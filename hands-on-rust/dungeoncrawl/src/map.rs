1b236b652081e7d41e6e8ec6551ed402d3037d88
Charlie Harrington <charles.harrington@gmail.com>
2022-01-27 11:10:30 -0800

    add moveable player

--- hands-on-rust/dungeoncrawl/src/map.rs
+++ hands-on-rust/dungeoncrawl/src/map.rs
@@ -36,4 +36,17 @@ impl Map {
             }
         }
     }
+    pub fn in_bounds(&self, point: Point) -> bool {
+        point.x >= 0 && point.x < SCREEN_WIDTH && point.y >= 0 && point.y < SCREEN_HEIGHT
+    }
+    pub fn can_enter_tile(&self, point: Point) -> bool {
+        self.in_bounds(point) && self.tiles[map_idx(point.x, point.y)] == TileType::Floor
+    }
+    pub fn try_idx(&self, point: Point) -> Option<usize> {
+        if !self.in_bounds(point) {
+            None
+        } else {
+            Some(map_idx(point.x, point.y))
+        }
+    }
 }
