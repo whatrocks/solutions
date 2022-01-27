0cbabdb3cc804ffc434134d9c888c57b9d8a51a7
Charlie Harrington <charles.harrington@gmail.com>
2022-01-26 20:58:53 -0800

    render blank dungeon map

--- hands-on-rust/dungeoncrawl/src/main.rs
+++ hands-on-rust/dungeoncrawl/src/main.rs
@@ -1,19 +1,35 @@
 mod map;
 
-use bracket_lib::prelude::*;
+mod prelude {
+    pub use bracket_lib::prelude::*;
+    pub const SCREEN_WIDTH: i32 = 80;
+    pub const SCREEN_HEIGHT: i32 = 50;
+    pub use crate::map::*;
+}
 
-struct State {}
+use prelude::*;
+
+struct State {
+    map: Map,
+}
+
+impl State {
+    fn new() -> Self {
+        Self { map: Map::new() }
+    }
+}
 
 impl GameState for State {
     fn tick(&mut self, ctx: &mut BTerm) {
         ctx.cls();
-        ctx.print(1, 1, "Hello, Bracket World!");
+        self.map.render(ctx);
     }
 }
 fn main() -> BError {
     let context = BTermBuilder::simple80x50()
         .with_title("Rusty Roguelike")
+        .with_fps_cap(30.0)
         .build()?;
 
-    main_loop(context, State {})
+    main_loop(context, State::new())
 }
