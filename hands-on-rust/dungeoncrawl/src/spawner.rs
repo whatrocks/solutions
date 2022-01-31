2371f7cb3a6581d8fc41c6e2c4c36dca41c71779
Charlie Harrington <charles.harrington@gmail.com>
2022-01-31 13:15:35 -0800

    monsters randomly move

--- hands-on-rust/dungeoncrawl/src/spawner.rs
+++ hands-on-rust/dungeoncrawl/src/spawner.rs
@@ -24,5 +24,6 @@ pub fn spawn_monster(ecs: &mut World, rng: &mut RandomNumberGenerator, pos: Poin
                 _ => to_cp437('g'),
             },
         },
+        MovingRandomly {},
     ));
 }
