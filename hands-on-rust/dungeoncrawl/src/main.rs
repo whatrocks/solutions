1e5bb2a8a360ee8e4a43bad8bc22a81839d635d5
Charlie Harrington <charles.harrington@gmail.com>
2022-02-22 09:16:11 -0800

    add items

--- hands-on-rust/dungeoncrawl/src/main.rs
+++ hands-on-rust/dungeoncrawl/src/main.rs
@@ -45,7 +45,7 @@ impl State {
         map_builder
             .monster_spawns
             .iter()
-            .for_each(|pos| spawn_monster(&mut ecs, &mut rng, *pos));
+            .for_each(|pos| spawn_entity(&mut ecs, &mut rng, *pos));
         resources.insert(map_builder.map);
         resources.insert(Camera::new(map_builder.player_start));
         resources.insert(TurnState::AwaitingInput);
@@ -70,7 +70,7 @@ impl State {
             .iter()
             .skip(1)
             .map(|r| r.center())
-            .for_each(|pos| spawn_monster(&mut self.ecs, &mut rng, pos));
+            .for_each(|pos| spawn_entity(&mut self.ecs, &mut rng, pos));
         self.resources.insert(map_builder.map);
         self.resources.insert(Camera::new(map_builder.player_start));
         self.resources.insert(TurnState::AwaitingInput);
