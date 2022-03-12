51528ad28f9a51222b8f2aff7b5d4a0994c003ef
Charlie Harrington <charles.harrington@gmail.com>
2022-03-11 16:06:33 -0800

    ownership

---
+++ rust-book/owners/src/main.rs
@@ -0,0 +1,22 @@
+fn main() {
+    let s1 = gives_ownership();
+    let s2 = String::from("hello");
+    let s3 = takes_and_gives_back(s2);
+    println!("{} {}", s3, s1);
+    let (_, s4length) = calculate_length(s3);
+    println!("length {}", s4length);
+}
+
+fn gives_ownership() -> String {
+    let some_string = String::from("yours");
+    some_string
+}
+
+fn takes_and_gives_back(a_string: String) -> String {
+    a_string
+}
+
+fn calculate_length(s: String) -> (String, usize) {
+    let length = s.len();
+    (s, length)   
+}
