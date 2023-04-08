d72e3acf62e00accc998d4aa73f8066fe81264d9
Charlie Harrington <charles.harrington@gmail.com>
2023-04-07 21:36:25 -0700

    updated beeper

--- csprimer/beep-beep-boop/beeper.py
+++ csprimer/beep-beep-boop/beeper.py
@@ -1,7 +1,31 @@
 import sys
 import tty
+import time
+import termios
+
+term_attrs = termios.tcgetattr(0)
+tty.setcbreak(0)
+
+def run():
+    while True:
+        ch = sys.stdin.read(1)
+        if '1' <= ch <= '9':
+            for _ in range(int(ch)):
+                #sys.stdout.buffer.write(b'\x07')
+                print('\a')
+                time.sleep(0.5)
+        #sys.stdout.buffer.flush()
+
+try:
+    run()
+finally:
+    termios.tcsetattr(0, termios.TCSADRAIN, term_attrs)
+
+"""
+import tty
 import termios
 import time
+ORIGINAL SOLUTION
 
 def read_one_byte(original_settings):
     try:
@@ -25,9 +49,11 @@ if __name__ == '__main__':
         if list(byte)[0] in [x for x in range(49, 58)]:
             n = list(byte)[0] - 48
             for i in range(n):
-                print('\a')
+                sys.stdout.buffer.write(b'\x07')
+                # print('\a')
                 time.sleep(0.25)
 
+"""
 """
 listen to stdin
 if is a number
